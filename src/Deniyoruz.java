c3ded875aea98e20b47d71ebbd9f6bd654daf151
cemalgndzz <cemal.gunduz@ug.bilkent.edu.tr>
2020-04-06 21:51:53 +0300

    Summation method implemented

--- src/Deniyoruz.java
+++ src/Deniyoruz.java
@@ -7,6 +7,12 @@ public class Deniyoruz {
         return a*b;
     }
 
+    public int summ(int a,int b)
+    {
+        return a+b;
+    }
+
+
     public static void main(String[] args)
     {
         System.out.println("Hello World");
