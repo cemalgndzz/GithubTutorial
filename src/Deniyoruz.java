88ccd810bc15603f070bcc15fffbec86865a6cb2
cemalgndzz <cemal.gunduz@ug.bilkent.edu.tr>
2020-04-06 21:59:20 +0300

    substraction method implemented

--- src/Deniyoruz.java
+++ src/Deniyoruz.java
@@ -17,6 +17,11 @@ public class Deniyoruz {
         return a/b;
     }
 
+    public int substraction(int a,int b)
+    {
+        return a-b;
+    }
+
     public static void main(String[] args)
     {
         System.out.println("Hello World");
